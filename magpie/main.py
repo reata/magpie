55eadf93e8fae102c1e0cabac2acdaa0b0c95a2a
reata <reddevil.hjw@gmail.com>
2022-06-09 23:45:19 +0800

    feat: add a new API for GitHub star history

--- magpie/main.py
+++ magpie/main.py
@@ -1,15 +1,17 @@
+from collections import Counter
+from datetime import datetime
+
 import httpx
 import uvicorn
+from dateutil.rrule import rrule, DAILY
 from fastapi import FastAPI
 from fastapi import Response
 from fastapi.middleware.cors import CORSMiddleware
+from pandas import DataFrame
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8000",
-    "https://reata.github.io"
-]
+origins = ["http://localhost:8000", "https://reata.github.io"]
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,15 +30,58 @@ async def root():
 @app.get("/api/pypistats/{pypistats_path:path}")
 async def pypistats(pypistats_path: str):
     async with httpx.AsyncClient() as client:
-        proxy: httpx.Response = await client.get(f"https://pypistats.org/{pypistats_path}")
-    return Response(content=proxy.content, status_code=proxy.status_code, media_type="application/json")
+        proxy: httpx.Response = await client.get(
+            f"https://pypistats.org/{pypistats_path}"
+        )
+    return Response(
+        content=proxy.content,
+        status_code=proxy.status_code,
+        media_type="application/json",
+    )
 
 
 @app.get("/api/github/{github_path:path}")
 async def github(github_path: str):
     async with httpx.AsyncClient() as client:
-        proxy: httpx.Response = await client.get(f"https://api.github.com/{github_path}")
-    return Response(content=proxy.content, status_code=proxy.status_code, media_type="application/json")
+        proxy: httpx.Response = await client.get(
+            f"https://api.github.com/{github_path}"
+        )
+    return Response(
+        content=proxy.content,
+        status_code=proxy.status_code,
+        media_type="application/json",
+    )
+
+
+@app.get("/api/starhistory/{repo:path}")
+async def starhistory(repo: str):
+    per_page = 100
+    page = 1
+    star_ts = []
+    async with httpx.AsyncClient() as client:
+        while True:
+            proxy: httpx.Response = await client.get(
+                f"https://api.github.com/repos/{repo}/stargazers?per_page={per_page}&page={page}",
+                headers={"Accept": "application/vnd.github.v3.star+json"},
+            )
+            ts = [stargazer["starred_at"] for stargazer in proxy.json()]
+            star_ts.extend(ts)
+            page += 1
+            if len(ts) < per_page:
+                break
+    star_dt = [datetime.strptime(ts, "%Y-%m-%dT%H:%M:%SZ").date() for ts in star_ts]
+    all_dt = list(
+        dt.date() for dt in rrule(DAILY, dtstart=star_dt[0], until=star_dt[-1])
+    )
+    no_star_dt = list(set(all_dt).difference(set(star_dt)))
+    df = DataFrame.from_records(
+        [(k, v) for k, v in Counter(star_dt).items()] + [(k, 0) for k in no_star_dt]
+    )
+    df.columns = ["date", "star_cnt"]
+    df = df.sort_values("date")
+    df["date"] = df["date"].apply(str)
+    df["star_cum_cnt"] = df["star_cnt"].cumsum()
+    return Response(content=df.to_json(orient="records"), media_type="application/json")
 
 
 if __name__ == "__main__":
