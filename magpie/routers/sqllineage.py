c42bbb2b19b019012f4c4d8518db205bbfaf36a8
reata <reddevil.hjw@gmail.com>
2022-10-22 22:34:16 +0800

    refactor: using sqllineage v1.3.7 api

--- magpie/routers/sqllineage.py
+++ magpie/routers/sqllineage.py
@@ -1,12 +1,10 @@
-from argparse import Namespace
-from pathlib import Path
-
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
-from sqllineage import DATA_FOLDER
-from sqllineage.runner import LineageRunner
-from sqllineage.utils.constant import LineageLevel
-from sqllineage.utils.helpers import extract_sql_from_args
+from sqllineage.drawing import (
+    directory as o_directory,
+    script as o_script,
+    lineage as o_lineage,
+)
 
 from magpie.models import Directory, SQLLineageInput
 
@@ -14,41 +12,18 @@ router = APIRouter(prefix="/api/sqllineage")
 
 
 @router.post("/directory")
-def directory(dir: Directory):
-    if dir.f:
-        dir_root = Path(dir.f).parent
-    elif dir.d:
-        dir_root = Path(dir.d)
-    else:
-        dir_root = Path(DATA_FOLDER)
-    data = {
-        "id": str(dir_root),
-        "name": dir_root.name,
-        "is_dir": True,
-        "children": [
-            {"id": str(p), "name": p.name, "is_dir": p.is_dir()}
-            for p in sorted(dir_root.iterdir(), key=lambda _: (not _.is_dir(), _.name))
-        ],
-    }
+def directory(_dir: Directory):
+    data = o_directory(_dir.dict())
     return JSONResponse(data)
 
 
 @router.post("/script")
 def script(sinput: SQLLineageInput):
-    req_args = Namespace(**sinput.dict())
-    sql = extract_sql_from_args(req_args)
-    return JSONResponse({"content": sql})
+    data = o_script(sinput.dict())
+    return JSONResponse(data)
 
 
 @router.post("/lineage")
 def lineage(sinput: SQLLineageInput):
-    req_args = Namespace(**sinput.dict())
-    sql = extract_sql_from_args(req_args)
-    lr = LineageRunner(sql, verbose=True)
-    return JSONResponse(
-        {
-            "verbose": str(lr),
-            "dag": lr.to_cytoscape(),
-            "column": lr.to_cytoscape(LineageLevel.COLUMN),
-        }
-    )
+    data = o_lineage(sinput.dict())
+    return JSONResponse(data)
