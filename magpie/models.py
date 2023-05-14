ad6cd092fdd14452ea68c25fa85a3ba494e7a676
reata <reddevil.hjw@gmail.com>
2023-05-14 11:02:28 +0800

    fix: sqllineage api not accepting dialect parameter

--- magpie/models.py
+++ magpie/models.py
@@ -4,8 +4,10 @@ from pydantic import BaseModel
 class Directory(BaseModel):
     f: str | None = None
     d: str | None = None
+    dialect: str = "non-validating"
 
 
 class SQLLineageInput(BaseModel):
     f: str | None = None
     e: str | None = None
+    dialect: str = "non-validating"
